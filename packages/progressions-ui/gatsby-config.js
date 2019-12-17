bbae4b58da0ddfb3fe04bc65c6ecbeff91be5e2e
TDSFugal <tim.smith@fugaltech.com>
2019-12-17 16:06:19 -0500

    Added redux as per gatsby example

--- packages/progressions-ui/gatsby-config.js
+++ packages/progressions-ui/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Music Progressions`,
+    description: `Explore music progressions with the circle of fifths.`,
+    author: `@tdsfugal`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
