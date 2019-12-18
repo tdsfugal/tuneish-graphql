a90f0bedbd0429343e6d7791a8289ba6b49b1faf
TDSFugal <tim.smith@fugaltech.com>
2019-12-18 14:19:07 -0500

    Initial circle of fifths in an svg pane

--- packages/progressions-ui/src/components/circle/circle.js
+++ packages/progressions-ui/src/components/circle/circle.js
@@ -1,5 +1,14 @@
 import React from "react"
 
 export default () => {
-  return <p>Foo</p>
+  const cx = 200
+  const cy = 200
+  const r = 160
+  const thick = 0.3
+  return (
+    <svg viewBox="0 0 600 600" xmlns="http://www.w3.org/2000/svg">
+      <circle cx={cx} cy={cy} r={r} fill="#44aaff" />
+      <circle cx={cx} cy={cy} r={r * (1 - thick)} fill="white" />
+    </svg>
+  )
 }
