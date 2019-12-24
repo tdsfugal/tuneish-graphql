7fc705bf69365efc42d00d387f76b339bcfbc0a3
TDSFugal <tim.smith@fugaltech.com>
2019-12-23 19:17:08 -0500

    Moved the origin of the SVG viewbox to the center of the box

--- packages/progressions-ui/src/components/circle/arc.js
+++ packages/progressions-ui/src/components/circle/arc.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const ATOR = Math.PI / 180
 
-export default ({ i, cx, cy, r_outer, r_inner }) => {
+export default ({ i, r_outer, r_inner }) => {
   const angle = i * 30
   const start_ang = (angle - 15) * ATOR
   const end_ang = (angle + 15) * ATOR
@@ -12,17 +12,17 @@ export default ({ i, cx, cy, r_outer, r_inner }) => {
   const s1 = Math.sin(end_ang)
   const c1 = Math.cos(end_ang)
 
-  const x1 = cx + r_inner * s0
-  const y1 = cy + r_inner * c0
+  const x1 = r_inner * s0
+  const y1 = r_inner * c0
 
-  const x2 = cx + r_inner * s1
-  const y2 = cy + r_inner * c1
+  const x2 = r_inner * s1
+  const y2 = r_inner * c1
 
-  const x3 = cx + r_outer * s1
-  const y3 = cy + r_outer * c1
+  const x3 = r_outer * s1
+  const y3 = r_outer * c1
 
-  const x4 = cx + r_outer * s0
-  const y4 = cy + r_outer * c0
+  const x4 = r_outer * s0
+  const y4 = r_outer * c0
 
   const inner = `A${r_inner} ${r_inner} 0  0,0 ${x2}, ${y2} `
   const right = `L${x3}, ${y3} `
--- packages/progressions-ui/src/components/circle/circle.js
+++ packages/progressions-ui/src/components/circle/circle.js
@@ -5,30 +5,14 @@ import ARC from "./arc"
 const THICK = 0.3
 const ARCS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
-export default ({ cx, cy, r }) => {
+export default ({ r }) => {
   const r_outer = r
   const r_inner = r * (1 - THICK)
   function makeArc(i) {
-    return <ARC i={i} cx={cx} cy={cy} r_outer={r_outer} r_inner={r_inner} />
+    return <ARC i={i} r_outer={r_outer} r_inner={r_inner} />
   }
   return (
-    <svg viewBox="0 0 600 600" xmlns="http://www.w3.org/2000/svg">
-      <circle
-        cx={cx}
-        cy={cy}
-        r={r_outer}
-        fill="none"
-        stroke="red"
-        stroke-width="2"
-      />
-      <circle
-        cx={cx}
-        cy={cy}
-        r={r_inner}
-        fill="none"
-        stroke="red"
-        stroke-width="2"
-      />
+    <svg viewBox="-200 -200 400 400" xmlns="http://www.w3.org/2000/svg">
       {ARCS.map(i => makeArc(i))}
     </svg>
   )
