baab572e44f75e92afab4c65f21bc42c8e5be733
TDSFugal <tim.smith@fugaltech.com>
2020-01-02 14:58:02 -0500

    Built a lookup function for notes by frequency.

--- packages/progressions-ui/src/theory/notes.js
+++ packages/progressions-ui/src/theory/notes.js
@@ -32,108 +32,131 @@ import {
   Gss,
 } from "./note-names"
 
-const A4 = 440 // Reference pitch, hz
-const semiTone = Math.pow(2, 1 / 12) // Even tempered
+import computeFrequencies from "./computeFrequencies"
 
-const An4 = A4
-const As4 = A4 * Math.pow(semiTone, 1)
-const Bn4 = A4 * Math.pow(semiTone, 2)
-const Cn4 = A4 * Math.pow(semiTone, 3)
-const Cs4 = A4 * Math.pow(semiTone, 4)
-const Dn4 = A4 * Math.pow(semiTone, 5)
-const Ds4 = A4 * Math.pow(semiTone, 6)
-const En4 = A4 * Math.pow(semiTone, 7)
-const Fn4 = A4 * Math.pow(semiTone, 8)
-const Fs4 = A4 * Math.pow(semiTone, 9)
-const Gn4 = A4 * Math.pow(semiTone, 10)
-const Gs4 = A4 * Math.pow(semiTone, 11)
+export default class Notes {
+  constructor() {
+    this._notes = [
+      {
+        tone: 0, // number of half steps up from C
+        names: [Cn, Bs, Dbb],
+        solfege: ["do"],
+      },
+      {
+        tone: 1,
+        names: [Cs, Db],
+        solfege: ["di", "ra"],
+      },
+      {
+        tone: 2,
+        names: [Dn, Css, Ebb],
+        solfege: ["re"],
+      },
+      {
+        tone: 3,
+        names: [Ds, Eb],
+        solfege: ["ri", "me"],
+      },
+      {
+        tone: 4,
+        names: [En, Css, Fb],
+        solfege: ["mi"],
+      },
+      {
+        tone: 5,
+        names: [Fn, Es, Gbb],
+        solfege: ["fa"],
+      },
+      {
+        tone: 6,
+        names: [Fs, Ess, Gb],
+        solfege: ["fi", "se"],
+      },
+      {
+        tone: 7,
+        names: [Gn, Fss, Abb],
+        solfege: ["sol"],
+      },
+      {
+        tone: 8,
+        names: [Gs, Ab],
+        solfege: ["si", "le"],
+      },
+      {
+        tone: 9,
+        names: [An, Gss, Bbb],
+        solfege: ["la"],
+      },
+      {
+        tone: 10,
+        names: [As, Bb],
+        solfege: ["le", "te"],
+      },
+      {
+        tone: 11,
+        names: [Bn, Ass, Cb],
+        solfege: ["ti"],
+      },
+    ]
+    this._freq = computeFrequencies()
+  }
 
-const computeFrequenciesFromFour = four => {
-  return [
-    four / 32,
-    four / 16,
-    four / 8,
-    four / 4,
-    four / 2,
-    four,
-    four * 2,
-    four * 4,
-    four * 8,
-    four * 16,
-  ]
+  getNoteByTone(tone) {
+    const note = this._notes[tone]
+    if (note)
+      return {
+        tone: note.tone,
+        names: note.names.slice(0),
+        solfege: note.solfege.slice(0),
+      }
+    return null
+  }
+
+  getNoteByMidi(midiNumber) {
+    const midi = this._freq[midiNumber]
+    const note = this._notes[midi.tone]
+    return {
+      tone: midi.tone,
+      oct: midi.oct,
+      freq: midi.freq,
+      names: note.names.slice(0),
+      solfege: note.solfege.slice(0),
+    }
+  }
+
+  getNearestMidi(freq) {
+    // Find the index i where:
+    //     freq <= freq[i].upper && freq > freq[i-1].upper
+    //
+    // then return the midi index i and the deviation from freq[i].freq
+    //
+    // The first step is to conduct a binary search of the frequencies to
+    // get close.  Often this will be the correct value.
+    //
+    let range = 144
+    let midpoint = range / 2
+    let delta = freq - this._freq[midpoint].freq
+    let newMid, newDelt
+    while (range > 3) {
+      newMid = Math.floor(
+        delta >= 0 ? midpoint + range / 4 : midpoint - range / 4
+      )
+      newDelt = freq - this._freq[newMid].freq
+      range = range / 2
+      if (Math.abs(newDelt) < Math.abs(delta)) {
+        delta = newDelt
+        midpoint = newMid
+      }
+    }
+    // Step 2 is to adjust the result using the exact .upper values
+    let index = midpoint
+    const upper = this._freq[midpoint].upper
+    const lower = midpoint > 0 ? this._freq[midpoint - 1].upper : 0
+    if (freq > upper) index += 1
+    if (freq < lower) index -= 1
+
+    // Finaly, compute the error
+    const correct = this._freq[index].freq
+    return { midi: index, error: correct - freq }
+  }
 }
-
-export default [
-  {
-    tone: 0, // number of half steps up from C
-    names: [Cn, Bs, Dbb],
-    sofledge: ["do"],
-    frequencies: computeFrequenciesFromFour(Cn4),
-  },
-  {
-    tone: 1,
-    names: [Cs, Db],
-    sofledge: ["di", "ra"],
-    frequencies: computeFrequenciesFromFour(Cs4),
-  },
-  {
-    tone: 2,
-    names: [Dn, Css, Ebb],
-    sofledge: ["re"],
-    frequencies: computeFrequenciesFromFour(Dn4),
-  },
-  {
-    tone: 3,
-    names: [Ds, Eb],
-    sofledge: ["ri", "me"],
-    frequencies: computeFrequenciesFromFour(Ds4),
-  },
-  {
-    tone: 4,
-    names: [En, Css, Fb],
-    sofledge: ["mi"],
-    frequencies: computeFrequenciesFromFour(En4),
-  },
-  {
-    tone: 5,
-    names: [Fn, Es, Gbb],
-    sofledge: ["fa"],
-    frequencies: computeFrequenciesFromFour(Fn4),
-  },
-  {
-    tone: 6,
-    names: [Fs, Ess, Gb],
-    sofledge: ["fi", "se"],
-    frequencies: computeFrequenciesFromFour(Fs4),
-  },
-  {
-    tone: 7,
-    names: [Gn, Fss, Abb],
-    sofledge: ["sol"],
-    frequencies: computeFrequenciesFromFour(Gn4),
-  },
-  {
-    tone: 8,
-    names: [Gs, Ab],
-    sofledge: ["si", "le"],
-    frequencies: computeFrequenciesFromFour(Gs4),
-  },
-  {
-    tone: 9,
-    names: [An, Gss, Bbb],
-    sofledge: ["la"],
-    frequencies: computeFrequenciesFromFour(An4),
-  },
-  {
-    tone: 10,
-    names: [As, Bb],
-    sofledge: ["le", "te"],
-    frequencies: computeFrequenciesFromFour(As4),
-  },
-  {
-    tone: 11,
-    names: [Bn, Ass, Cb],
-    sofledge: ["ti"],
-    frequencies: computeFrequenciesFromFour(Bn4),
-  },
-]
---
+++ packages/progressions-ui/src/theory/notes.test.js
@@ -0,0 +1,102 @@
+import Notes from "./notes"
+import {
+  Abb,
+  Bbb,
+  Dbb,
+  Ebb,
+  Gbb,
+  Ab,
+  Bb,
+  Cb,
+  Db,
+  Eb,
+  Fb,
+  Gb,
+  An,
+  Bn,
+  Cn,
+  Dn,
+  En,
+  Fn,
+  Gn,
+  As,
+  Bs,
+  Cs,
+  Ds,
+  Es,
+  Fs,
+  Gs,
+  Ass,
+  Css,
+  Ess,
+  Fss,
+  Gss,
+} from "./note-names"
+
+describe("Notes", () => {
+  it("compiles", () => {
+    const foo = new Notes()
+    expect(foo).toBeInstanceOf(Notes)
+  })
+
+  it("returns note names by tone", () => {
+    const foo = new Notes()
+    const cases = [
+      [0, [Cn, Bs, Dbb], ["do"]],
+      [1, [Cs, Db], ["di", "ra"]],
+      [2, [Dn, Css, Ebb], ["re"]],
+      [3, [Ds, Eb], ["ri", "me"]],
+      [4, [En, Css, Fb], ["mi"]],
+      [5, [Fn, Es, Gbb], ["fa"]],
+      [6, [Fs, Ess, Gb], ["fi", "se"]],
+      [7, [Gn, Fss, Abb], ["sol"]],
+      [8, [Gs, Ab], ["si", "le"]],
+      [9, [An, Gss, Bbb], ["la"]],
+      [10, [As, Bb], ["le", "te"]],
+      [11, [Bn, Ass, Cb], ["ti"]],
+    ]
+
+    cases.map(x => {
+      const [tone, names, solfege] = x
+      const y = foo.getNoteByTone(tone)
+      expect(y.tone).toEqual(tone)
+      expect(y.names).toEqual(names)
+      expect(y.solfege).toEqual(solfege)
+    })
+  })
+
+  it("Gets notes by midi number", () => {
+    const foo = new Notes()
+    const c4 = foo.getNoteByMidi(60)
+    expect(c4.oct).toBe(4)
+    expect(c4.freq).toBeCloseTo(261.626, 3)
+    expect(c4.names).toEqual([Cn, Bs, Dbb])
+    expect(c4.solfege).toEqual(["do"])
+
+    const a4 = foo.getNoteByMidi(69)
+    expect(a4.tone).toBe(9)
+    expect(a4.oct).toBe(4)
+    expect(a4.freq).toBeCloseTo(440, 3)
+    expect(a4.names).toEqual([An, Gss, Bbb])
+    expect(a4.solfege).toEqual(["la"])
+  })
+
+  it("Finds the note nearest a frequency", () => {
+    const foo = new Notes()
+    const cases = [
+      [16.352, 12],
+      [415, 68],
+      [427.47, 68],
+      [427.52, 69],
+      [440, 69],
+      [12544, 127],
+    ]
+    cases.map(x => {
+      const [freq, correctMidi] = x
+      const correctFreq = foo.getNoteByMidi(correctMidi).freq
+      const y = foo.getNearestMidi(freq)
+      expect(y.midi).toEqual(correctMidi)
+      expect(y.error).toBeCloseTo(correctFreq - freq, 5)
+    })
+  })
+})
