4efe09da6f41ae1f4501ebfebac2877c42cf9bb5
TDSFugal <tim.smith@fugaltech.com>
2020-01-08 00:07:25 -0500

    Broke fretboard into HOC and view

---
+++ packages/progressions-ui/src/components/fretboard/fretboard-view.js
@@ -0,0 +1,67 @@
+import React from "react"
+
+const STRING_THICKNESS = 4
+const FRET_THICKNESS = 1
+const BOARD_COLOR = "#420"
+
+const String = ({ yPos, length }) => {
+  return (
+    <rect
+      x="0"
+      y={(yPos - STRING_THICKNESS / 2).toString()}
+      width={length}
+      height={STRING_THICKNESS.toString()}
+      fill="#AAA"
+    />
+  )
+}
+
+const Fret = ({ xPos, boardWidth }) => {
+  return (
+    <rect
+      x={xPos.toString()}
+      y="0"
+      width={FRET_THICKNESS.toString()}
+      height={boardWidth}
+      fill="#933"
+    />
+  )
+}
+
+export default ({
+  tuning,
+  boardLength,
+  boardWidth,
+  stringPositions,
+  fretPositions,
+}) => {
+  // Compute the frets
+  const frets = fretPositions.map((xPos, fret) => {
+    return <Fret key={`f_${fret}`} xPos={xPos} boardWidth={boardWidth} />
+  })
+
+  // Compute the strings
+  const strings = stringPositions.map((yPos, string) => {
+    return <String key={`s_${string}`} yPos={yPos} length={boardLength} />
+  })
+
+  // Compute the notes
+
+  // Render the package deal
+  return (
+    <div width="100%">
+      <svg viewBox="0 0 1000 200" xmlns="http://www.w3.org/2000/svg">
+        <rect
+          key="board"
+          x="0"
+          y="0"
+          width={boardLength}
+          height={boardWidth}
+          fill={BOARD_COLOR}
+        />
+        {frets}
+        {strings}
+      </svg>
+    </div>
+  )
+}
--- packages/progressions-ui/src/components/fretboard/fretboard.js
+++ packages/progressions-ui/src/components/fretboard/fretboard.js
@@ -2,32 +2,10 @@ import React from "react"
 
 import FRET_POSITIONS from "../../theory/fret-positions"
 
+import FretboardView from "./fretboard-view"
+
 const STRING_SPACING = 30
 const EDGE_MARGIN = 10
-
-const String = ({ yPos, thick, length }) => {
-  return (
-    <rect
-      x="0"
-      y={(yPos - thick / 2).toString()}
-      width={length}
-      height={thick.toString()}
-      fill="#AAA"
-    />
-  )
-}
-
-const Fret = ({ xPos, thick, boardWidth }) => {
-  return (
-    <rect
-      x={xPos.toString()}
-      y="0"
-      width={thick.toString()}
-      height={boardWidth}
-      fill="#933"
-    />
-  )
-}
 
 const computeFretPosition = (fret, scaleLength) => {
   return scaleLength * FRET_POSITIONS[fret]
@@ -38,47 +16,31 @@ const computeStringPosition = string => {
 }
 
 export default ({ tuning, nFrets }) => {
-  const length = 1000
+  const boardLength = 1000
   const boardWidth = 2 * EDGE_MARGIN + STRING_SPACING * (tuning.length - 1)
-  const color = "#420"
 
   // Compute the frets
-  const frets = []
-  const scaleLength = length / FRET_POSITIONS[nFrets]
+  const fretPositions = []
+  const scaleLength = boardLength / FRET_POSITIONS[nFrets]
   for (let fret = 1; fret < nFrets + 1; fret++) {
-    const thick = 1 // Base on the note.  TODO
-    const xPos = computeFretPosition(fret, scaleLength)
-    const key = `f_${fret}`
-    frets.push(
-      <Fret key={key} xPos={xPos} thick={thick} boardWidth={boardWidth} />
-    )
+    fretPositions.push(computeFretPosition(fret, scaleLength))
   }
 
   // Compute the strings
-  const strings = tuning.map((note, string) => {
-    const thick = 4 // Base on the note.  TODO
-    const yPos = computeStringPosition(string)
-    const key = `s_${note}`
-    return <String key={key} yPos={yPos} thick={thick} length={length} />
+  const stringPositions = tuning.map((note, string) => {
+    return computeStringPosition(string)
   })
 
   // Compute the notes
 
   // Render the package deal
   return (
-    <div width="100%">
-      <svg viewBox="0 0 1000 200" xmlns="http://www.w3.org/2000/svg">
-        <rect
-          key="wood"
-          x="0"
-          y="0"
-          width={length}
-          height={boardWidth}
-          fill={color}
-        />
-        {frets}
-        {strings}
-      </svg>
-    </div>
+    <FretboardView
+      tuning={tuning}
+      boardLength={boardLength}
+      boardWidth={boardWidth}
+      stringPositions={stringPositions}
+      fretPositions={fretPositions}
+    />
   )
 }
