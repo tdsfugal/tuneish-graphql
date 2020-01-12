798217f96c26f5db05fb967768a51bd6ee3d71da
TDSFugal <tim.smith@fugaltech.com>
2020-01-11 23:02:32 -0500

    Added get note midis by name to notes singleton

--- packages/progressions-ui/src/theory/notes.js
+++ packages/progressions-ui/src/theory/notes.js
@@ -27,6 +27,7 @@ import {
   Gs,
   Ass,
   Css,
+  Dss,
   Ess,
   Fss,
   Gss,
@@ -41,61 +42,73 @@ export default class Notes {
         tone: 0, // number of half steps up from C
         names: [Cn, Bs, Dbb],
         solfege: ["do"],
+        midis: [0, 12, 24, 36, 48, 60, 72, 84, 96, 108, 120],
       },
       {
         tone: 1,
         names: [Cs, Db],
         solfege: ["di", "ra"],
+        midis: [1, 13, 25, 37, 49, 61, 73, 85, 97, 109, 121],
       },
       {
         tone: 2,
         names: [Dn, Css, Ebb],
         solfege: ["re"],
+        midis: [2, 14, 26, 38, 50, 62, 74, 86, 98, 110, 122],
       },
       {
         tone: 3,
         names: [Ds, Eb],
         solfege: ["ri", "me"],
+        midis: [3, 15, 27, 39, 51, 63, 75, 87, 99, 111, 123],
       },
       {
         tone: 4,
-        names: [En, Css, Fb],
+        names: [En, Dss, Fb],
         solfege: ["mi"],
+        midis: [4, 16, 28, 40, 52, 64, 76, 88, 100, 112, 124],
       },
       {
         tone: 5,
         names: [Fn, Es, Gbb],
         solfege: ["fa"],
+        midis: [5, 17, 29, 41, 53, 65, 77, 89, 101, 113, 125],
       },
       {
         tone: 6,
         names: [Fs, Ess, Gb],
         solfege: ["fi", "se"],
+        midis: [6, 18, 30, 42, 54, 66, 78, 90, 102, 114, 126],
       },
       {
         tone: 7,
         names: [Gn, Fss, Abb],
         solfege: ["sol"],
+        midis: [7, 19, 31, 43, 55, 67, 79, 91, 103, 115, 127],
       },
       {
         tone: 8,
         names: [Gs, Ab],
         solfege: ["si", "le"],
+        midis: [8, 20, 32, 44, 56, 68, 80, 92, 104, 116],
       },
       {
         tone: 9,
         names: [An, Gss, Bbb],
         solfege: ["la"],
+        midis: [9, 21, 33, 45, 57, 69, 81, 93, 105, 117],
       },
       {
         tone: 10,
         names: [As, Bb],
         solfege: ["le", "te"],
+        midis: [10, 22, 34, 46, 58, 70, 82, 94, 106, 118],
       },
       {
         tone: 11,
         names: [Bn, Ass, Cb],
         solfege: ["ti"],
+        midis: [11, 23, 35, 47, 59, 71, 83, 95, 107, 119],
       },
     ]
     this._freq = computeFrequencies()
@@ -108,8 +121,21 @@ export default class Notes {
         tone: note.tone,
         names: note.names.slice(0),
         solfege: note.solfege.slice(0),
+        midis: note.midis.slice(0), // TODO - Is all this stuff always neccesary? could be leaner and faster.
       }
     return null
+  }
+
+  getMidisByName(name) {
+    let tone, i
+    for (tone = 0; tone < 12; tone++) {
+      const names = this._notes[tone].names
+      for (i = 0; i < names.length; i++) {
+        if (name === names[i]) {
+          return this._notes[tone].midis.slice(0)
+        }
+      }
+    }
   }
 
   getNoteByMidi(midiNumber) {
