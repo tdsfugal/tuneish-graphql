23964f18cad6dbaa06e7b36a30c8f752858760f6
TDSFugal <tim.smith@fugaltech.com>
2020-02-14 08:35:16 -0500

    Refactored graphql to be topic based

--- packages/svr-gql/src/graphql/index.js
+++ packages/svr-gql/src/graphql/index.js
@@ -1,2 +1,1 @@
-export { default as schema } from "./schema"
-export { default as resolvers } from "./resolvers"
+export * from "./users"
--- packages/svr-gql/src/graphql/users.js
+++ packages/svr-gql/src/graphql/users.js
@@ -3,7 +3,20 @@ import { gql } from "apollo-server-express"
 import { getLogger } from "../util"
 const logger = getLogger(__filename)
 
-const schema = gql`
+export const resolvers = {
+  Mutation: {
+    subscribe: async (_, { firstName, lastName, email }, { subscriptions }) => {
+      const subscription = await subscriptions.subscribe({
+        firstName,
+        lastName,
+        email,
+      })
+      return subscription
+    },
+  },
+}
+
+export const schema = gql`
   type Subscription {
     firstName: String!
     lastName: String!
@@ -24,5 +37,3 @@ const schema = gql`
     unsubscribe(email: String!): Subscription
   }
 `
-
-export default schema
