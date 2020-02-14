e2cf6c0f907b5c19bfa9b379966e537849444b6c
TDSFugal <tim.smith@fugaltech.com>
2020-02-14 08:39:10 -0500

    Introduced tunes topic to graphql

--- packages/svr-gql/src/graphql/index.js
+++ packages/svr-gql/src/graphql/index.js
@@ -1,4 +1,5 @@
 import { usersResolvers, usersSchema } from "./users"
+import { tunesResolvers, tunesSchema } from "./tunes"
 
 export { usersResolvers as resolvers }
 export { usersSchema as schema }
---
+++ packages/svr-gql/src/graphql/tunes.js
@@ -0,0 +1,8 @@
+import { gql } from "apollo-server-express"
+
+import { getLogger } from "../util"
+const logger = getLogger(__filename)
+
+export const tunesResolvers = {}
+
+export const tunesSchema = null
