f83242aeddc849711bfea8076bc2b969c89cb5b8
TDSFugal <tim.smith@fugaltech.com>
2020-02-16 02:10:57 -0500

    Updated dependencies in app.

--- packages/app-main/src/state/apollo/client.js
+++ packages/app-main/src/state/apollo/client.js
@@ -1,11 +1,29 @@
-import ApolloClient from "apollo-boost"
+import { ApolloClient } from "apollo-client"
+import { InMemoryCache } from "apollo-cache-inmemory"
+import { HttpLink } from "apollo-link-http"
+import { onError } from "apollo-link-error"
+import { ApolloLink } from "apollo-link"
 import fetch from "isomorphic-fetch"
 
 const API_URI = process.env.GATSBY_API_URI || "http://localhost:3000/api/v1"
 
 const apolloClient = new ApolloClient({
-  uri: API_URI,
-  fetch,
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.forEach(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          )
+        )
+      if (networkError) console.log(`[Network error]: ${networkError}`)
+    }),
+    new HttpLink({
+      uri: API_URI,
+      fetch,
+    }),
+  ]),
+  cache: new InMemoryCache(),
 })
 
 export default apolloClient
