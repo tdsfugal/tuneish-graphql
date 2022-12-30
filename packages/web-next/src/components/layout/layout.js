788d4dcaaddb806ec7d011a33ce6f61ed5ef1a3a
TDSFugal <tim.smith@fugaltech.com>
2022-12-30 17:15:02 -0500

    global css

--- packages/web-next/src/components/layout/layout.js
+++ packages/web-next/src/components/layout/layout.js
@@ -1,8 +1,25 @@
 import React from "react";
+import { Global, css } from "@emotion/react";
+
 import { LayoutView } from "/src/styles";
 
-const Layout = ({ children }) => {
-  return <LayoutView>{children}</LayoutView>;
-};
+const GLOBAL_STYLES = css`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  .body {
+    background-color: #ffffff;
+  }
+`;
+
+const Layout = ({ children }) => (
+  <>
+    <Global styles={GLOBAL_STYLES} />
+    <LayoutView>{children}</LayoutView>
+  </>
+);
 
 export default Layout;
--- packages/web-next/src/styles/layout-views.js
+++ packages/web-next/src/styles/layout-views.js
@@ -1,15 +1,19 @@
 import styled from "@emotion/styled";
 
 export const LayoutView = styled.div`
+  position: absolute;
   display: flex;
   flex-flow: column nowrap;
-  position: absolute;
-  height: 100vh;
-  width: 100vw;
+  height: 100%;
+  width: 100%;
   margin: 0;
   padding: 0;
+  justify-content: start;
+  align-items: center;
   overflow: hidden;
   background-color: #ffffff;
+  color: #000000;
+  font-familyL -apple-system, Roboto, sans-serif, serif;
 `;
 
 export const RowView = styled.div`
