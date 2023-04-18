32b2f8797ffccc3bc087e7fd6a4a9111ab77ad72
TDSFugal <tim.smith@fugaltech.com>
2023-04-17 23:05:50 -0400

    Dynamic load circle svg

--- packages/web-next/src/components/circle/index.js
+++ packages/web-next/src/components/circle/index.js
@@ -1,1 +1,10 @@
-export { default as Circle } from "./circle";
+import dynamic from "next/dynamic";
+
+const DynamicCircle = dynamic(() => import("./circle"), {
+  loading: () => null,
+  ssr: false,
+});
+
+export function Circle({ r }) {
+  return <DynamicCircle r={r} />;
+}
