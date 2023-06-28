2401c033f3599797f352aa07f49bc09879b1881a
TDSFugal <tim.smith@fugaltech.com>
2023-06-27 23:36:37 -0400

    fixup: typo in calc

--- packages/web-next/src/styles/client/drawer-bar-views.js
+++ packages/web-next/src/styles/client/drawer-bar-views.js
@@ -3,17 +3,19 @@ import styled from "styled-components";
 import { getDimensionToken } from "src/design";
 
 const DrawerBarView = styled.div`
-  width: calc(100vw-${getDimensionToken(["layout", "nav_width"])});
-  position: relative;
+  width: calc(100vw - ${getDimensionToken(["layout", "nav_width"])});
+  position: absolute;
   z-index: 100;
 `;
 
 export const HeaderDrawerView = styled(DrawerBarView)`
   height: ${getDimensionToken(["layout", "header_height"])};
-  background-color: yellow;
+  background-color: rgb(200, 200, 0, 0.3);
+  top: true;
 `;
 
 export const FooterDrawerView = styled(DrawerBarView)`
   height: ${getDimensionToken(["layout", "footer_height"])};
-  background-color: green;
+  background-color: rgb(200, 0, 200, 0.3);
+  bottom: true;
 `;
