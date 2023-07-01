3e6f7ea63a6d3b602020ec4b2624859bf5777297
TDSFugal <tim.smith@fugaltech.com>
2023-07-01 16:13:05 -0400

    tun-14: mouse down home links

--- packages/web-next/src/components/client/home-links-item.js
+++ packages/web-next/src/components/client/home-links-item.js
@@ -5,7 +5,7 @@ import { SCROLL_TO } from "src/state/reactive";
 
 const HomeLinksItem = ({ _id, label }) => {
   const handleClick = (e) => {
-    e.preventDefault();
+    // do not prevent default; the links container has event handlers too
     SCROLL_TO(_id);
   };
   return (
--- packages/web-next/src/components/client/home-links.js
+++ packages/web-next/src/components/client/home-links.js
@@ -11,8 +11,17 @@ import HomeLinksItem from "./home-links-item";
 const HomeLinks = () => {
   const manifest = useReactiveVar(HOME_MANIFEST);
 
+  const handleMouseDown = (e) => {
+    console.log(e);
+    const en = e.nativeEvent;
+    if (en.buttons == 2) {
+      // secondary mouse button pressed, e.g. right click
+      console.log("right mouse click, y offset = ", en.offsetY);
+    }
+  };
+
   return (
-    <HomeLinksView className="HomeLinks">
+    <HomeLinksView className="HomeLinks" onMouseDown={handleMouseDown}>
       {manifest.map(({ _id, label }) => (
         <HomeLinksItem key={_id} _id={_id} label={label} />
       ))}
