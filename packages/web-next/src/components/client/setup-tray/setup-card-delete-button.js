efa1cb8232f8d28053f99338395936a0cea0ee1a
TDSFugal <tim.smith@fugaltech.com>
2023-07-06 19:38:12 -0400

    tun-16: delete works

---
+++ packages/web-next/src/components/client/setup-tray/setup-card-delete-button.js
@@ -0,0 +1,32 @@
+"use client";
+
+import { useReactiveVar } from "@apollo/client";
+
+import { ACTIVE_SETUP_CARD, HOME_MANIFEST } from "src/state/reactive";
+
+import { SetupCardHeaderButtonView } from "./setup-tray-views";
+
+const SetupCardDeleteButton = ({ itemId }) => {
+  const manifest = useReactiveVar(HOME_MANIFEST);
+
+  const handleDelete = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    // update the manifest
+    const newManifest = manifest.filter(({ _id }) => _id !== itemId);
+    HOME_MANIFEST(newManifest);
+    // close the card
+    ACTIVE_SETUP_CARD(null);
+  };
+
+  return (
+    <SetupCardHeaderButtonView
+      className="SetupCardDeleteButton"
+      onClick={handleDelete}
+    >
+      DELETE
+    </SetupCardHeaderButtonView>
+  );
+};
+
+export default SetupCardDeleteButton;
--- packages/web-next/src/components/client/setup-tray/setup-card.js
+++ packages/web-next/src/components/client/setup-tray/setup-card.js
@@ -8,8 +8,9 @@ import {
   SetupCardView,
   SetupCardHeaderView,
   SetupCardBodyView,
-  SetupCardHeaderButtonView,
 } from "./setup-tray-views";
+
+import SetupCardDeleteButton from "./setup-card-delete-button";
 
 const SetupCard = ({ card }) => {
   const manifest = useReactiveVar(HOME_MANIFEST);
@@ -22,31 +23,19 @@ const SetupCard = ({ card }) => {
 
     // insert the new item at position pos without deleting any others, and update the value of
     // the reactive var. Note that the root pointer has to change for the reactive var to react
-    // so regular splice doesn't work.
-
-    // The _id values need to be unique, otherwise key collisions in main and elsewhere.
-    HOME_MANIFEST(
-      manifest.toSpliced(pos, 0, {
-        _id: Math.random().toString(),
-        label: "new thing",
-      })
-    );
-  };
-
-  const handleDelete = (e) => {
-    e.preventDefault();
-    console.log("delete ", _id);
+    // so regular splice doesn't work. The _id values need to be unique, otherwise there will be
+    // key collisions in main and elsewhere.
+    const newManifest = manifest.toSpliced(pos, 0, {
+      _id: Math.random().toString(),
+      label: "new thing",
+    });
+    HOME_MANIFEST(newManifest);
   };
 
   return (
     <SetupCardView className="SetupCard" yPos={yPos} onClick={handleClick}>
       <SetupCardHeaderView className="SetupCardHeader">
-        <SetupCardHeaderButtonView
-          className="SetupCardDeleteButton"
-          onClick={handleDelete}
-        >
-          DELETE
-        </SetupCardHeaderButtonView>
+        <SetupCardDeleteButton itemId={_id} />
       </SetupCardHeaderView>
       <SetupCardBodyView className="SetupCardBody">
         {_id ? `Item at manifest ${pos}` : `New item for manifest pos ${pos}`}
