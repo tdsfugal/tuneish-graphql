44d5d5499795cf35bf6ac096d06237d979f00181
TDSFugal <tim.smith@fugaltech.com>
2023-07-05 23:30:47 -0400

    tun-15: cleanup home

--- packages/web-next/src/components/layout/main/home/home.js
+++ packages/web-next/src/components/layout/main/home/home.js
@@ -1,19 +1,14 @@
 import React from "react";
-import { useReactiveVar } from "@apollo/client";
 
 import { MainWrapper } from "../../page-views";
 
 import { HomeBackgroundView } from "./home-views";
-
-import { HOME_MANIFEST } from "src/state/reactive";
 
 import { HomeScroll, SetupTray } from "src/components/client";
 
 import { Footer, Header } from "../_marginals";
 
 const Home = () => {
-  const manifest = useReactiveVar(HOME_MANIFEST);
-
   return (
     <MainWrapper className="MainWrapper">
       <HomeBackgroundView className="HomeBackground" />
