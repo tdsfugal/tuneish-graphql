1e9f1d24cc1b673c4b55d1824ce3b64b7ba7c6e0
TDSFugal <tim.smith@fugaltech.com>
2023-07-06 03:19:42 -0400

    tun-15: add works.  15 done

--- packages/web-next/src/components/client/setup-tray/setup-card.js
+++ packages/web-next/src/components/client/setup-tray/setup-card.js
@@ -1,15 +1,31 @@
 "use client";
 
-import { ACTIVE_SETUP_CARD } from "src/state/reactive";
+import { useReactiveVar } from "@apollo/client";
+
+import { ACTIVE_SETUP_CARD, HOME_MANIFEST } from "src/state/reactive";
 
 import { SetupTrayCardView } from "./setup-tray-views";
 
 const SetupTrayCard = ({ card }) => {
+  const manifest = useReactiveVar(HOME_MANIFEST);
+
   const { yPos, _id, pos } = card;
 
   const handleClick = (e) => {
     e.preventDefault();
     ACTIVE_SETUP_CARD(null);
+
+    // insert the new item at position pos without deleting any others, and update the value of
+    // the reactive var. Note that the root pointer has to change for the reactive var to react
+    // so regular splice doesn't work.
+
+    // The _id values need to be unique, otherwise key collisions in main and elsewhere.
+    HOME_MANIFEST(
+      manifest.toSpliced(pos, 0, {
+        _id: Math.random().toString(),
+        label: "new thing",
+      })
+    );
   };
 
   return (
