3576308d4cb57540194639a9d39e06ab1a63527e
TDSFugal <tim.smith@fugaltech.com>
2023-07-06 20:17:31 -0400

    tun-15: 15 is done

--- packages/web-next/src/state/reactive/home.js
+++ packages/web-next/src/state/reactive/home.js
@@ -1,11 +1,5 @@
 import { makeVar } from "@apollo/client";
 
-export const HOME_MANIFEST = makeVar([
-  { _id: "1", label: "one" },
-  { _id: "2", label: "two" },
-  { _id: "3", label: "three" },
-  { _id: "4", label: "four" },
-  { _id: "5", label: "five" },
-]);
+export const HOME_MANIFEST = makeVar([]);
 
 export const SCROLL_TO = makeVar("");
